435380c8043c160e7c1ec8c9bcd4372368b64f33
Ignacio Fonts <fontsignacio@gmail.com>
2022-12-28 15:08:42 -0300

    Http Handler

---
+++ billboard_movies/lib/common/http_handler.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HttpHandler{
+  
+  Future<dynamic> getJson(Uri uri) async{
+    http.Response response = await http.get(uri);
+    return json.decode(response.body).toString();
+  }
+}
+
