23a4facfe034323c0d4cbc6897bfb4698bec8c67
Ignacio Fonts <fontsignacio@gmail.com>
2022-12-31 19:17:57 -0300

    Movie Information

--- billboard_movies/lib/media_list_item.dart
+++ billboard_movies/lib/media_list_item.dart
@@ -10,12 +10,61 @@ class MediaListItem extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          FadeInImage.assetNetwork(
-            placeholder: "assets/placeholder.jpg",
-            image: media.getBackDropUrl(),
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 300,
+          Stack(
+            children: <Widget>[
+              FadeInImage.assetNetwork(
+                placeholder: "assets/placeholder.jpg",
+                image: media.getBackDropUrl(),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 300,
+              ),
+              Positioned(
+                left: 0.0,
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900]?.withOpacity(0.5),
+                  ),
+                  constraints: const  BoxConstraints.expand(
+                    height: 55.0
+                  ),
+                ),          
+              ),
+              Positioned(
+                left: 10.0,
+                bottom: 10.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(media.title, 
+                    style: const TextStyle(fontWeight: FontWeight.bold,
+                    color: Colors.white
+                    )
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: Text(media.getGenres(), 
+                        style: const TextStyle(color: Colors.white),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                    ),
+                  )],
+                ),
+              ),
+              Positioned(
+                  left: 400.0,
+                  bottom: 10.0,
+                  child: Row(
+                    children: [
+                      Text(media.releaseDate, 
+                      style: const TextStyle(color: Colors.white)),
+                      const Icon(Icons.calendar_month, size: 17)
+                    ],
+                  )
+              )
+            ],
           )
         ],
       ),
