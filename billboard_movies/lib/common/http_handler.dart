291326f0d9103921795e8a96a6e497e1b10f6067
Ignacio Fonts <fontsignacio@gmail.com>
2023-01-08 17:37:27 -0300

    Upcoming page

--- billboard_movies/lib/common/http_handler.dart
+++ billboard_movies/lib/common/http_handler.dart
@@ -24,5 +24,18 @@ class HttpHandler{
       data['results'].map<Media>((item) => Media(item)).toList()
     );
   }
+
+  Future<List<Media>> fetchUpcoming(){
+    var uri = Uri.https(_baseUrl, "3/movie/upcoming",{
+      'api_key' : API_KEY,
+      'page' : "1",
+      'language' : _language
+    });     
+
+    return getJson(uri).then((data) => 
+      data['results'].map<Media>((item) => Media(item)).toList()
+    );
+  }
+  
 }
 
--- billboard_movies/lib/pages/coming_page.dart
+++ billboard_movies/lib/pages/coming_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:billboard_movies/common/http_handler.dart';
+import 'package:billboard_movies/model/media.dart';
+import 'package:billboard_movies/media_list_item.dart';
 
-class Coming extends StatelessWidget {
+
+class Coming extends StatefulWidget {
   const Coming({super.key});
 
   @override
+  State<Coming> createState() => _ComingState();
+}
+
+class _ComingState extends State<Coming> {
+  final List<Media> _media = [];
+
+  @override
+  void initState(){
+    super.initState();
+    loadMovies();
+  }
+  void loadMovies()async{
+    var movies = await HttpHandler().fetchUpcoming();
+    setState(() {
+      _media.addAll(movies);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: _media.length,
+      itemBuilder:  (context, index){
+        return MediaListItem(media: _media[index]);
+      }     
+    );
   }
 }
