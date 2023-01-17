918afa323dd855ee68fcdea61d692ce945d7c92e
Ignacio Fonts <fontsignacio@gmail.com>
2023-01-17 12:55:32 -0300

    Search Style

--- billboard_movies/lib/pages/search.dart
+++ billboard_movies/lib/pages/search.dart
@@ -10,6 +10,36 @@ class User extends StatefulWidget {
 class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Serch");
+    return Scaffold(
+      body: Container(
+        height: 60,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color:const Color(0xff292b37),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Icon(Icons.search,
+              color: Colors.white54,
+              size: 30
+            ),
+            Container(
+              width: 400,
+              margin: const EdgeInsets.only(left: 5),
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search",
+                  hintStyle: TextStyle(color: Colors.white54)
+                ),
+              ),
+            )
+          ],
+        ),            
+      ),
+    );
   }
 }
